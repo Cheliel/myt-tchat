39dd543062f8bc86704375fad75bd55b08d20f0c
Cheliel <etienne.pernon@gmail.com>
2023-06-22 23:23:54 +0200

    CHX =>  afficahge console

--- src/utils/socket/messageHandeler.tsx
+++ src/utils/socket/messageHandeler.tsx
@@ -2,14 +2,23 @@
 
 
 export const onMessageHandler = (e: any) => {
+
+    console.log(" <============> type: ", e.type, " <=============>")  
+    console.log("|")
+    console.log("|")
+
     const message : string = e.data
     if(typeof message === 'string'){
         if (message.includes('PRIVMSG')) {
             const parts = message?.split('PRIVMSG');
             const sender = parts[0].split('!')[0].slice(1);
             const content = parts[1].trim();
-            console.log('Expéditeur :', sender);
-            console.log('Contenu du message :', content);
+            console.log('| Expéditeur :', sender);
+            console.log('| Contenu du message :', content);
           }
-    }   
+    } 
+    console.log("|")
+    console.log("|")
+    console.log("|==============================================>")
+    //oauth:9kccggpp7bg3cbplilm3m98zryai6x
 }
